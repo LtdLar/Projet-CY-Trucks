c640a4f5c23933cb44ab13a81088cf607c43406f
LtdLar <94563153+LtdLar@users.noreply.github.com>
2024-01-16 14:33:11 +0100

    Add files via upload

--- D1.sh
+++ D1.sh
@@ -1,14 +1,24 @@
 #!/bin/bash 
 
+file="data.csv"
+
+
 Start=$(date +%s)
 touch output.sh
 touch debara.sh
 touch new.dat
-
+touch names.dat
+touch res.dat
 
 sort -r -o output.sh data.csv
 cut -d ";" -f 1,6 output.sh > debara.sh
-tail -10 debara.sh > new.dat
+cut -d ";" -f 6 output.sh > names.dat
+
+for i in "names.dat";do
+	grep -c "names.dat"[i] data.csv > res.dat
+done
+
+tail -10 output.sh > new.dat
 
 
 gnuplot gd1.gnu
@@ -16,4 +26,5 @@ gnuplot gd1.gnu
 End=$(date +%s)
 echo "Temps d'execution $(($End-$Start)) sec"
 
-#regarder cmb de personnes ont le plus de route ID
+#problème de valeur, vérifier new.dat
+
--- h.sh
+++ h.sh
@@ -1,3 +1,23 @@
 #!/bin/bash 
 
-echo "Besoin d'aide ?"
+echo "Besoin d'aide ?
+    utilisez -h, sinon -D1; -D2 ou -exit"
+
+
+for arg in "$@" ; do
+   shift
+   case "$arg" in
+      --D1) echo "Ici on va mettre le programme D1 !"
+      ;;
+      --D2) echo "Ici on va mettre le programme D2 !"
+      ;;
+      --S) echo "Ici on va mettre le programme S !"
+      ;;
+      --h) echo "Aide"
+      ;;
+      *) echo "aucune option détectée"
+         exit 1
+      ;;
+   esac
+done
+
