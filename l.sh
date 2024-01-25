1533c8276fa223e09f47b7fb6f6b80487063c2f3
leslieahf <153763800+leslieahf@users.noreply.github.com>
2024-01-25 01:16:57 +0100

    Update l.sh

--- l.sh
+++ l.sh
@@ -1,5 +1,13 @@
+Start=$(date+%s)
+
 awk -F';' '{tab[$1]+=$5} END {for (i in tab) printf "%d;%d\n", i, tab[i]}' data.csv | sort -t';' -r -n -k2 | head -10 | sort -t';' -r -n -k1 > datadistance.csv
 cat datadistance.csv
+gnuplot l.gnu
+
+End=$(date +%s)
+echo "Temps d'execution $(($End-$Start)) sec"
+
+#affichage des 10 trajets les plus longs en fonction de l'identifiant du trajet
 
 295579;2500
 256594;2323
