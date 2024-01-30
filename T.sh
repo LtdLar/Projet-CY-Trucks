a1957cdf6bca1d776a1aabb650d44bb49de57d76
Ocenyna <131042956+Ocenyna@users.noreply.github.com>
2024-01-30 20:16:28 +0100

    LE traitement T mais sans gnuplot

---
+++ T.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+Start=$(date +%s)
+# Vérifie si le nombre d'arguments est correct
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 fichier_csv"
+    exit 1
+fi
+
+# Récupère le fichier CSV en argument
+fichier_csv="$1"
+
+# Vérifie si le fichier existe
+if [ ! -f "$fichier_csv" ]; then
+    echo "Le fichier $fichier_csv n'existe pas."
+    exit 1
+fi
+
+# Trie la troisième colonne du fichier CSV
+trier_colonne=$(sort -t';' -k3,3 "$fichier_csv")
+
+# Compte les occurrences des mots dans la troisième colonne
+occurrences=$(echo "$trier_colonne" | cut -d';' -f3 | uniq -c)
+
+# Trie les occurrences dans l'ordre décroissant
+tri_occurrences=$(echo "$occurrences" | sort -nr)
+
+# Sélectionne les 10 premières lignes (les mots les plus fréquents)
+top_10_occurrences=$(echo "$tri_occurrences" | head -n 10)
+
+# Crée un nouveau fichier CSV pour stocker les résultats
+nouveau_fichier_dat="resultats.dat"
+
+# Affiche les résultats dans le nouveau fichier CSV
+echo "$top_10_occurrences" > "$nouveau_fichier_dat"
+
+echo "Les 10 mots les plus fréquents ont été enregistrés dans $nouveau_fichier_dat."
+
+
+#Affichage du tmps d'éxecution
+End=$(date +%s)
+echo "Temps d'execution $(($End-$Start)) sec"
